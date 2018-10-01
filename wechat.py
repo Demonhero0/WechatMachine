f78e9f6c6d3f778fb03b2f74d0dec9c1f2aa25b2
Sunyh7 <Sunyh7@users.noreply.github.com>
2018-10-01 21:59:38 +0800

    22：00

--- wechat.py
+++ wechat.py
@@ -1,6 +1,10 @@
 import itchat
 import time
 import xlrd
+import xlwt
+
+send = 1
+state = 0
 
 def read():
     file = xlrd.open_workbook('二班名单.xlsx')#formatting_info=True
@@ -12,6 +16,8 @@ def read():
         temp = {'number':number,'name':name,'date':None}
         data.append(temp)
     return data
+
+data = read()
 
 def save(data,path,sheet,row,col):
     xls = xlrd.open_workbook(path)
@@ -28,14 +34,16 @@ def getChatroom(name):
         if room['NickName'] == name:
             return room['UserName']
 
-send = 1
-
 @itchat.msg_register(itchat.content.TEXT,isGroupChat=True)
 def LabourBot(msg):
+    global send
+    global state
     hour = time.localtime(time.time())[3]
-    if hour >= 19 and send:
-        itchat.send_msg(msg='Text Message',toUserName=getChatroom('求求你们都别学了'))
+    if hour >= 19 and send == 1:
+        message = data[state]['name'] + '和' + data[state + 1]['name'] + '同学，明天将轮到你们擦黑板。请回复“我会好好擦黑板”，否则将视作缺勤。'
+        itchat.send_msg(msg=message,toUserName=getChatroom('求求你们都别学了'))
         send = 0
+        state = (state + 2) % 53
     if hour < 19:
         send = 1
     print(msg['Text'])
@@ -43,5 +51,4 @@ def LabourBot(msg):
 #    if(msg['ActualUserName']==&&msg['Text']=='我会好好擦黑板')
 itchat.auto_login(enableCmdQR = True, hotReload = True)
 #print(itchat.search_chatrooms(name="数院2017级二班"))
-
 itchat.run()
